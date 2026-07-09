3f5a506226af1e90d71c90e52be02cd9131f4442
NeelChat-17 <neelanjanchatterjee17@gmail.com>
2026-07-09 19:40:28 +0530

    created class GraphProblem

---
+++ graph.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class GraphProblem:
+    def graphing(self, edges, N, src):
+        print("in the class")
+    
+if __name__ == "__main__":
+    gp = GraphProblem()
+    gp.graphing(12, 3, 0)
+
+        
