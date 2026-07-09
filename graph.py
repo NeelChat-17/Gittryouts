6ac315a61daac0ec826946302df5d1b720a75914
NeelChat-17 <neelanjanchatterjee17@gmail.com>
2026-07-09 21:02:23 +0530

    modified: added BFS to find smallest path

--- graph.py
+++ graph.py
@@ -22,14 +22,28 @@ class GraphProblem:
         q = []
         q.append(src)
 
-        print(graph)
-        print(dist)
-        print(q)
+        while q:
+            node = q.pop()
+            for i in graph[node]:
+                if dist[node]+1 < dist[i]:
+                    dist[i] = dist[node]+1
+                    q.append(i)
+
+        for i in range(N):
+            if dist[i] == float('inf'):
+                dist[i] = -1
+            
+        return dist
         
     
 if __name__ == "__main__":
     gp = GraphProblem()
-    edges = [[1,0],[2,1],[0,3],[3,7],[3,4],[7,4],[7,6],[4,5],[4,6],[6,5]] 
-    gp.graphing(edges, 3, 0)
+    edges = [[1,0],[2,1],[0,3],[3,7],[3,4],[7,4],[7,6],[4,5],[4,6],[6,5]]
+    N = 8
+    M = 10
+    src = 0 
+    result = gp.graphing(edges, N, src)
+    print(" ".join(map(str,result)))
+
 
         
